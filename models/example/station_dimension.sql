40767dad6cacef0c8a5ca53afa81788b51a75530
Simpy Tiwari <tiwarisimpy08@gmail.com>
2026-04-24 18:43:21 +0000

    station_dimension model created

---
+++ models/example/station_dimension.sql
@@ -0,0 +1,14 @@
+
+with bike as 
+(
+select 
+distinct START_STATIO_ID AS station_id,
+start_station_name as station_name,
+START_LAT as station_lat,
+START_LNG as start_station_lng 
+from {{ source('demo', 'bike') }}
+where ride_id != 'ride_id'
+
+)
+select *
+from bike
